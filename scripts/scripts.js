ca256529121e1189e7c9ace334a78553552cc8ea
CoburnFuse <mail.stankaandorp@gmail.com>
2025-01-30 20:11:13 +0100

    Time to fix bugs #5

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,13 +2,13 @@ var onField = [];
 var darkThemeOn;
 var combatantInitiative;
 
-function addNewOnField(event){
+function addNewOnField(event) {
     event.preventDefault();
 
     combatantInitiative = parseFloat(inputInitiative.value).toFixed(1);
     // Get all variables
-    if (onField.some(onField => onField.initiative === combatantInitiative) === false ){
-        onField.push({name: inputName.value, health: inputHP.value, initiative: combatantInitiative, isPlayer: inputPlayerBool.checked, currentTurn: false});
+    if (onField.some(onField => onField.initiative === combatantInitiative) === false) {
+        onField.push({ name: inputName.value, health: inputHP.value, initiative: combatantInitiative, isPlayer: inputPlayerBool.checked, currentTurn: false });
 
         // Clear form
         addNew.reset();
@@ -18,7 +18,7 @@ function addNewOnField(event){
     }
 }
 
-function updateTable(){
+function updateTable() {
     // Empty the table to rewrite
     emptyTable();
     sortByInitiative();
@@ -27,22 +27,22 @@ function updateTable(){
 
     // Get the tbody of the table and write to table
     var tbodyRef = document.getElementById('combatTracker').getElementsByTagName('tbody')[0];
-    for (var i = 0; i < onField.length; i++){
+    for (var i = 0; i < onField.length; i++) {
         tbodyRef.insertRow().innerHTML =
-        "<td>" + ((onField[i].currentTurn) ? '►':'') + onField[i].name + "</td>" +
-        "<td>" + ((onField[i].isPlayer) ? '' : '<input class="healthTable" type="number" value = ' + onField[i].health + ' id="hp_' + i + '" onchange="updateHP(' + i + ')">') + "</td>" +
-        "<td>" + onField[i].initiative + "</td>" +
-        "<td> <button onclick='removeSingleFromTracker(" + i + ");'>Kill</button></td>";
+            "<td>" + ((onField[i].currentTurn) ? '►' : '') + onField[i].name + "</td>" +
+            "<td>" + ((onField[i].isPlayer) ? '' : '<input class="healthTable" type="number" value = ' + onField[i].health + ' id="hp_' + i + '" onchange="updateHP(' + i + ')">') + "</td>" +
+            "<td>" + onField[i].initiative + "</td>" +
+            "<td> <button onclick='removeSingleFromTracker(" + i + ");'>Kill</button></td>";
     }
 }
 
-function sortByInitiative(){
-    onField.sort(function (a,b){
+function sortByInitiative() {
+    onField.sort(function (a, b) {
         return b.initiative - a.initiative
     });
 }
 
-function updateHP(id){
+function updateHP(id) {
     var newHealth = document.getElementById("hp_" + id).value;
     onField[id].health = parseInt(newHealth);
 
@@ -51,60 +51,62 @@ function updateHP(id){
     saveToStorage();
 }
 
-function emptyTracker(){
+function emptyTracker() {
     // Empty the array
     let text = "Are you sure you want to clear the tracker?";
-    if (confirm(text) === true){
+    if (confirm(text) === true) {
         onField.length = 0;
         updateTable();
     }
 }
 
-function emptyTable(){
+function emptyTable() {
     document.getElementById('combatants').innerHTML = '';
 }
 
-function removeSingleFromTracker(id){
+function removeSingleFromTracker(id) {
     onField.splice(id, 1);
     updateTable();
 }
 
-function nextInTurn(){
-    var getCurrentTurn = onField.findIndex(onField => onField.currentTurn === true);
+function nextInTurn() {
+    if (onField.length !== 0) {
+        var getCurrentTurn = onField.findIndex(onField => onField.currentTurn === true);
 
-    if (getCurrentTurn !== -1) {
-        onField[getCurrentTurn].currentTurn = false;
+        if (getCurrentTurn !== -1) {
+            onField[getCurrentTurn].currentTurn = false;
+        }
+
+        var getNextTurn = (getCurrentTurn + 1) % onField.length;
+        onField[getNextTurn].currentTurn = true;
+
+        updateTable();
     }
-
-    var getNextTurn = (getCurrentTurn + 1) % onField.length;
-    onField[getNextTurn].currentTurn = true;
-
-    updateTable();
 }
 
-function saveToStorage(){
+function saveToStorage() {
     localStorage.currentCombatants = JSON.stringify(onField);
 }
 
-function loadStorage(){
-    if (localStorage.getItem("currentCombatants") != null){
+function loadStorage() {
+    if (localStorage.getItem("currentCombatants") != null) {
         onField = JSON.parse(localStorage.currentCombatants);
     }
-    
-    if (localStorage.getItem("currentTheme") != null){
+
+    if (localStorage.getItem("currentTheme") != null) {
         darkThemeOn = JSON.parse(localStorage.currentTheme);
-        if(darkThemeOn === true){
+        if (darkThemeOn === true) {
             document.body.classList.add("dark");
         }
-    }   
+    }
     updateTable();
 }
 
-function switchTheme(){
-    if(darkThemeOn === true){
+function switchTheme() {
+    if (darkThemeOn === true) {
         document.body.classList.remove("dark");
         darkThemeOn = false;
-    }else{
+    } else {
         document.body.classList.add("dark");
         darkThemeOn = true;
     }
@@ -112,10 +114,10 @@ function switchTheme(){
     localStorage.currentTheme = JSON.stringify(darkThemeOn);
 }
 
-function checkForCombat(){
-    if(onField.find(item => item.currentTurn === true)) {
+function checkForCombat() {
+    if (onField.find(item => item.currentTurn === true)) {
         document.getElementById("turnButton").textContent = "Next in turn!";
-    }else{
+    } else {
         document.getElementById("turnButton").textContent = "Start combat!";
     }
 }
